a94cf3560b86a5a3626cbe9369793038aa498c1d
MYKBRU\willy <medicalawn@gmail.com>
2019-11-19 16:06:58 +0700

    munculkan icon

--- src/Main.js
+++ src/Main.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import {createStackNavigator} from 'react-navigation-stack';
 import {createAppContainer, createSwitchNavigator} from 'react-navigation';
 import HomeScreen from './containers/HomeScreen';
@@ -6,6 +7,8 @@ import ContactScreen from './containers/ContactScreen';
 import Config from 'react-native-config';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
 import {createDrawerNavigator} from 'react-navigation-drawer';
+import { createMaterialBottomTabNavigator } from "react-navigation-material-bottom-tabs";
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const HomeStack = createStackNavigator(
   {
@@ -17,6 +20,69 @@ const HomeStack = createStackNavigator(
     initialRouteName: 'HomeScreen',
     headerMode: 'none',
   },
+);
+
+
+const MainTabs2 = createMaterialBottomTabNavigator(
+  {
+    HomeScreen: {
+      screen: HomeScreen,
+      navigationOptions: {
+        tabBarLabel: 'Beranda',
+        tabBarOptions:{
+          showIcon: true,
+        },
+      },
+    },
+    AboutScreen: {
+      screen: AboutScreen,
+      navigationOptions: {
+        tabBarLabel: 'Keranjang',
+      },
+    },
+    ContactScreen: {
+      screen: ContactScreen,
+      navigationOptions: {
+        tabBarLabel: 'Polis',
+      },
+    },
+    AkunScreen: {
+      screen: ContactScreen,
+      navigationOptions: {
+        tabBarLabel: 'Akun',
+      },
+    },
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
+        let IconComponent = Ionicons;
+        let iconName;
+        if (routeName === 'HomeScreen') {
+          iconName = `ios-information-circle${focused ? '' : '-outline'}`;
+          // Sometimes we want to add badges to some icons.
+          // You can check the implementation below.
+          // IconComponent = HomeIconWithBadge;
+        } else  {
+          iconName = `ios-options`;
+        }
+
+        // You can return any component that you like here!
+        return <IconComponent name={iconName} size={25} color={tintColor} />;
+      },
+    }),
+    initialRouteName: 'HomeScreen',
+    shifting: false,
+    labeled:true,
+    activeColor:'#FF0000',
+    labelStyle:{
+      fontSize:12,
+    },
+    barStyle:{
+      backgroundColor:'#FFFFFF',
+    },  
+  }
 );
 
 const MainTabs = createBottomTabNavigator(
@@ -56,11 +122,12 @@ const MainTabs = createBottomTabNavigator(
         backgroundColor: '#FFFFFF',
       },
     },
+
   },
 );
 
 const MainDrawer = createDrawerNavigator({
-  MainTabs: MainTabs,
+  MainTabs2: MainTabs2,
   HomeStack: HomeStack,
 });
 
--- src/containers/HomeScreen/index.js
+++ src/containers/HomeScreen/index.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {View, Text, Button} from 'react-native';
 import Header from './header';
+import Icon from 'react-native-vector-icons/dist/Ionicons';
 
 export default class HomeScreen extends Component {
   constructor(props) {
@@ -14,6 +15,9 @@ export default class HomeScreen extends Component {
         <View style={{height:56}}><Header /></View>
         <View style={{flex:1, alignItems:'center'}}>
           <Text> Homepage </Text>
+        </View>
+        <View style={{flex:1}}>
+          <Icon name='logo-facebook' size={20} />
         </View>
         <View style={{flex:1, marginLeft:'10%', marginRight:'10%'}}>
           <Button
